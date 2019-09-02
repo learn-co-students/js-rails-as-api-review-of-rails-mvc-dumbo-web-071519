a89fdb04cb9d023ffb6856d13af65d432ab968b5
jason <jasonedlewis@gmail.com>
2019-09-01 20:25:04 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,8 +2,9 @@
 class BirdsController < ApplicationController
 
     def index
-        @birds = Bird.all
-        render 'birds/index.html.erb'
+        birds = Bird.all
+        render json: birds
+        
     end
     
 end
